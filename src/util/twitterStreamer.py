bd9a86952cf20b9fd1d457ccfc784ff09523f360
jcatfour <jcatfour@gmail.com>
2017-06-20 17:09:12 -0400

    added file streaming, extracting more info

--- src/util/twitterStreamer.py
+++ src/util/twitterStreamer.py
@@ -6,11 +6,23 @@ class MyStreamListener(tweepy.StreamListener):
   def __init__(self):
     print("Streamer object created")
     self.outputFile = open("output.txt", 'ab')
+    self.status_count = 0
   
   def on_status(self, status):
-    #print(json.loads(status)["text"])
-    self.outputFile.write((status.text + "\n").encode("UTF-8"))
+    self.status_count += 1
+    self.outputFile.write(self.getStatusInfo(status))
+    print("wrote status", self.status_count)
     
   def setAPI(self, api):
     self.api = api
+    
+  def getStatusInfo(self, status): # implement geo enabled logic
+    info = "Number: "     + str(self.status_count) + "\n" + \
+           "author: "     + str(status.author.screen_name) + "\n" + \
+           "text: "       + str(status.text) + "\n" + \
+           "language: "   + str(status.author.lang) + "\n" + \
+           "location: "   + str(status.author.location) + "\n" + \
+           "time zone: "  + str(status.author.time_zone) + "\n" + \
+           "created at: " + str(status.created_at) + "\n\n"
+    return info.encode("UTF-8")
     
